5ddf52b81a7038b91ab94b121693119b114fe4c8
gamzerol <erol.gmze@gmail.com>
2025-02-03 15:51:07 +0300

    NextAuth setup

--- db/sample-data.ts
+++ db/sample-data.ts
@@ -1,4 +1,20 @@
+import { hashSync } from 'bcrypt-ts-edge';
+
 const sampleData = {
+  users: [
+    {
+      name: 'Gamze',
+      email: 'gamze@example.com',
+      password: hashSync('123456',10),
+      role: 'admin'
+    },
+    {
+      name: 'Burak',
+      email: 'burak@example.com',
+      password: hashSync('123456',10),
+      role: 'user'
+    }
+  ],
   products: [
     {
       name: 'Polo Sporting Stretch Shirt',
