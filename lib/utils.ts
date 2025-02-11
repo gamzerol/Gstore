e6858f0c859765abaa3ebd23dfd1fb6c89343527
gamzerol <erol.gmze@gmail.com>
2025-02-11 14:05:11 +0300

    Sign up page and error handling

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,8 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
 
 //Convert prisma object into a regular JS object
@@ -12,6 +12,27 @@ export function convertToPlainObject<T>(value: T): T {
 
 //Format number with decimal places
 export function formatNumberWithDecimal(num: number): string {
-  const [int, decimal] = num.toString().split('.');
-  return decimal ? `${int}.${decimal.padEnd(2, '0')}` : `${int}.00`
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
+}
+
+//Format errors
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export default function formatError(error: any) {
+  if (error.name === "ZodError") {
+    const fieldErrors = Object.keys(error.errors).map(
+      (field) => error.errors[field].message
+    );
+    return fieldErrors.join(". ");
+  } else if (
+    error.name === "PrismaClientKnownRequestError" &&
+    error.code === "P2002"
+  ) {
+    const field = error.meta?.target ? error.meta.target[0] : "Field";
+    return `${field.charAt(0).toUpperCase() + field.slice(1)} already exists`;
+  } else {
+    return typeof error.message === "string"
+      ? error.message
+      : JSON.stringify(error.message);
+  }
 }
